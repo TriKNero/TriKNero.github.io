fcea0d372c73281fd8048203d299e80f6b2307e6
Kristian Korneev <krisnero@yandex.ru>
2019-03-31 23:50:52 +0300

    Updates

---
+++ precache-manifest.655646abea62dd3572f9801c797e905a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hi/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3adabc44864922305b69",
+    "url": "/hi/static/js/runtime~main.f07b1f74.js"
+  },
+  {
+    "revision": "cd317a3a5c8bccc5a6a5",
+    "url": "/hi/static/js/main.d8f457aa.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/hi/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "cd317a3a5c8bccc5a6a5",
+    "url": "/hi/static/css/main.7acf1f93.chunk.css"
+  },
+  {
+    "revision": "f6452019bf5b204ad1aff0e99028a44a",
+    "url": "/hi/index.html"
+  }
+];
