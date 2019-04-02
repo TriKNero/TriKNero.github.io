aead9772ce157b25c16d606d0e0e161342c24e69
Kristian Korneev <krisnero@yandex.ru>
2019-04-02 22:19:09 +0300

    Updates

---
+++ precache-manifest.077c013176e9895d8cdebf0424439943.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac6a078414bc084d2fb24dd30417b45d",
+    "url": "/static/media/vk.ac6a0784.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "619231ff8ec7a6b4d39aede30fa9890e",
+    "url": "/static/media/linkedin.619231ff.svg"
+  },
+  {
+    "revision": "662a4f1472618eb2eb3b507d7459741f",
+    "url": "/static/media/instagram.662a4f14.svg"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/static/media/github.617870e4.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "0ae4bee442dca0a699c6",
+    "url": "/static/js/main.5999c0ec.chunk.js"
+  },
+  {
+    "revision": "e89efa8683d9386d0f94",
+    "url": "/static/js/2.d237b223.chunk.js"
+  },
+  {
+    "revision": "0ae4bee442dca0a699c6",
+    "url": "/static/css/main.1770ab7e.chunk.css"
+  },
+  {
+    "revision": "c8d01d1da9df8dbe1ce28c97ef09f3ff",
+    "url": "/index.html"
+  }
+];
